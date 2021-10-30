3f464aca867356ab3bbcf81a45b0466f3e1f2403
Danila <danilka.eremin@gmail.com>
2021-10-30 21:24:45 +0800

    two

--- app/src/main/java/com/example/myinvestmentportfolio/MainActivity.kt
+++ app/src/main/java/com/example/myinvestmentportfolio/MainActivity.kt
@@ -3,11 +3,26 @@ package com.example.myinvestmentportfolio
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myinvestmentportfolio.ui.theme.MyInvestmentPortfolioTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +32,7 @@ class MainActivity : ComponentActivity() {
             MyInvestmentPortfolioTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MyScreenContent()
                 }
             }
         }
@@ -25,14 +40,99 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyScreenContent(){
+    Scaffold(topBar={
+        TopAppBar(title = {Text(text = "My Wallet")},
+            actions = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = "")
+                }
+            },
+            backgroundColor= Color.White,
+            elevation = 0.dp,
+            navigationIcon= {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(painter = painterResource(id = R.drawable.ic_baseline_person_24),
+                        contentDescription = "")
+                }
+            }
+        )
+    }) {
+        Column(modifier= Modifier
+            .fillMaxWidth()
+            .padding(top = 10.dp),
+            horizontalAlignment= Alignment.CenterHorizontally,
+        ) {
+            PersonalAccount()
+            MyPortfolio()
+        }
+    }
 }
+
+@Composable
+fun PersonalAccount(){
+    Box(modifier = Modifier.size(360.dp, 190.dp)) {
+        Card(backgroundColor = Color.Blue,
+            modifier = Modifier.size(360.dp, 160.dp),
+            shape = RoundedCornerShape(20.dp)
+        ) {
+            Column(horizontalAlignment= Alignment.CenterHorizontally
+                , modifier = Modifier.padding(top=35.dp)) {
+                Text(text = "US$5,657.60"
+                    , fontWeight = FontWeight.Bold
+                    , color = Color.White, fontSize = 30.sp
+                    , modifier = Modifier.padding(bottom = 10.dp))
+                Text(text = "6:00 PM Friday, Oct 14, 2021"
+                    , color = Color.White)
+                
+            }
+        }
+
+        Box(
+            Modifier
+                .align(Alignment.BottomCenter)
+                .size(240.dp, 60.dp)
+        ){
+            Card(shape = RoundedCornerShape(20.dp)
+                ,modifier = Modifier.matchParentSize(),elevation = 10.dp) {
+                Row(verticalAlignment=Alignment.CenterVertically
+                    ,horizontalArrangement=Arrangement.SpaceAround) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Icon(painter = painterResource(id = R.drawable.ic_baseline_history_24)
+                            , contentDescription = ""
+                            , modifier= Modifier.size(40.dp))
+                        Text(text = "History", fontSize = 12.sp,textAlign = TextAlign.Center)
+                    }
+
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Icon(painter = painterResource(id = R.drawable.ic_baseline_share_24)
+                            , contentDescription = ""
+                            , modifier= Modifier.size(40.dp))
+                        Text(text = "Share", fontSize = 12.sp,textAlign = TextAlign.Center)
+                    }
+
+                }   
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MyPortfolio(){
+    LazyRow(modifier = Modifier.padding(top = 40.dp)){
+        items(100){
+            Text(text = "Item: $it")
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyInvestmentPortfolioTheme {
-        Greeting("Android")
+        MyScreenContent()
     }
 }
+
